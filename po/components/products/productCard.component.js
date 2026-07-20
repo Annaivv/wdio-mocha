43d3673aced7805d0529c2dc56f640a6ec0f1ff2
annaivv <annashalya@gmail.com>
2026-07-20 15:44:19 +0200

    fix: rootEl selector added for product card

--- po/components/products/productCard.component.js
+++ po/components/products/productCard.component.js
@@ -1,7 +1,10 @@
 import { BaseComponent } from "../common/base.component";
 
 export class ProductCard extends BaseComponent {
+  constructor() {
+    super("app-detail");
+  }
   get addToCartBtn() {
-    return $('button[data-test="add-to-cart"]');
+    return this.rootEl.$('button[data-test="add-to-cart"]');
   }
 }
